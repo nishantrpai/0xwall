af8d65a5bfe2e1b65fe5a844e96864586651bc9d
Nishant <nishantrpai@gmail.com>
2023-01-11 18:02:16 +0530

    Update unlock.js

--- extension/js/unlock.js
+++ extension/js/unlock.js
@@ -34,7 +34,7 @@ async function unlockPayWall() {
     document.getElementById("checkout-price").innerText = "Processing";
     document.getElementById("install-mm").innerText = "Don't refresh the page";
     document.getElementById("lock-svg").innerHTML =
-      '<svg stroke="#eee" fill="none" stroke-width="1" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="5em" width="5em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 9.9-1"></path></svg>';
+      '<svg stroke="#ccc" fill="none" stroke-width="1" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="5em" width="5em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 9.9-1"></path></svg>';
     const receipt = await tx.wait();
     if (receipt) {
       let addTxResp = await fetch(":api_url:/api/reader", {
